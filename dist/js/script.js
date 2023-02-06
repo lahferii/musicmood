98cc2cce463cfb65af1efbe022e6ca9062ff4e60
feri-nurjaman04 <ferinaise@gmail.com>
2023-02-06 17:48:11 +0800

    sorry, lagi galau

--- dist/js/script.js
+++ dist/js/script.js
@@ -16,18 +16,34 @@ function popupCard(){
 
 const play = document.getElementById('button-play');
 const songWrapper = document.getElementById('song-wrapper');
-play.addEventListener('click', playMusic);
+const imgWrapper = document.getElementById('img-wrapper');
+const img = document.getElementById('img')
+let lagu = new Audio('./dist/audio/lagu.mp3');
 
-function playMusic(){
-  box.classList.toggle('animate-cardIn');
-  box.classList.toggle('animate-cardOut');
-
+play.addEventListener('click', function(){
+  box.classList.toggle('animate-cardIn');  
+  
+  setTimeout(function(){
+    box.classList.toggle('animate-cardOut');
+  }, 50)
+  
   setTimeout(function(){
     card.classList.toggle('hidden');
     card.classList.toggle('flex');
-  }, 450)
+    
+  }, 500)
   setTimeout(function(){
+    lagu.play();
     songWrapper.classList.toggle('hidden');
     songWrapper.classList.toggle('flex');
-  }, 500)
-}
+    imgWrapper.classList.toggle('animate-fadeIn');
+    setTimeout(function(){
+      img.classList.toggle('animate-pulse')
+    }, 6500)
+  }, 1000)
+
+  setTimeout(function(){
+    alert('dasar anak kontol')
+    document.location = 'index.html'
+  }, 29000)
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
+  content: ['./index.html', './**/*.{html, css, js}'],
   theme: {
     extend: {
       animation: {
